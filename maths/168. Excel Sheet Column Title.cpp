edca8a5a31eb10fec0acb03c7ad485dcd0c4d529
Ayesha Khan <91973186+Ayesha2002@users.noreply.github.com>
2024-07-24 15:57:21 +0530

    Create 168. Excel Sheet Column Title.cpp

---
+++ maths/168. Excel Sheet Column Title.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string str="";
+        while (columnNumber >0){
+            char ch = char((columnNumber-1)%26+65);
+            str =ch+str;
+            columnNumber = (columnNumber-1)/26;
+
+        }
+        return str;
+    }
+};
